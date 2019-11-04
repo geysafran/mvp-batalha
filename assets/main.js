2ab29121db57443844d62b6fb0c670ba092c8919
geysafran <geysafrancaromao@gmail.com>
2019-11-04 17:33:34 -0200

    adicionei hover e linkei as sections

--- assets/main.js
+++ assets/main.js
@@ -1,41 +1,41 @@
 let imglist = [
-    'desenho.jpg',
-    'evelynduda.jpg',
-    'DSC_0894.jpg',
-    'DSC_1438.jpg',
-    'DSC_1283.jpg',
-    '/SC_0635.jpg',
-    'vavamimi.jpg'
-]
-
+    'assets/mudin.jpg',
+    'assets/mudin.jpg',
+    'assets/mudin.jpg',
+    'assets/mudin.jpg',
+    'assets/mudin.jpg',
+    'assets/mudin.jpg',
+    'assets/mudin.jpg'
+];
 let counter = 0; 
 
-let corpo = document.querySelector('box-img-home');
+let corpin = document.querySelector('box-img-home');
 
-
+// Imagem
 let img = document.createElement('img');
 img.setAttribute('src', imglist [counter] );
 
 
 
-
-let before = document.createElement('h1');
+// Setinha de voltar
+let before = document.createElement('h4');
 let beforeText = document.createTextNode('<');
 before.appendChild(beforeText);
 
-
-let next = document.createElement('h1');
+// Setinha de adiantar
+let next = document.createElement('h4');
 let nextText = document.createTextNode('>');
 next.appendChild(nextText);
 
 
     function init() {
-    
-        corpo.appendChild(before);
-        corpo.appendChild(img);
-        corpo.appendChild(next);
+        
+        corpin.appendChild(before);
+        corpin.appendChild(img);
+        corpin.appendChild(next);
         
     }
+///before
 before.addEventListener('click', function() {
     if (counter > 0 ){
         counter = counter - 1;
@@ -46,7 +46,7 @@ before.addEventListener('click', function() {
 
    
 })
-
+///next
 next.addEventListener('click', function() {
     if (counter < 6 ){
         counter = counter + 1;
